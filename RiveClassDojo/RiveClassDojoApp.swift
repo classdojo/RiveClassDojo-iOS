e561ea0a4e3aa89133834aa6a716ecf1813b1dee
Jakub Kaspar <kaspikk@gmail.com>
2024-05-29 17:01:11 +0200

    Initial Commit

---
+++ RiveClassDojo/RiveClassDojoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveClassDojoApp.swift
+//  RiveClassDojo
+//
+//  Created by Jakub Kašpar on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RiveClassDojoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
